28886ac92ed772a2480a4c56f33b8008522aea09
Dunkelweizen <cai.nowicki@gmail.com>
2020-11-05 00:18:56 -0500

    finished histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,23 @@
-# Your code here
+import sys
+sys.path.append('../')
+from word_count.word_count import word_count
 
+with open("robin.txt") as f:
+    words = f.read()
+
+for word in words:
+    word = word.lower()
+
+word_dict = word_count(words)
+longest = 0
+for word in word_dict.keys():
+    if len(word) > longest:
+        longest = len(word)
+        longest_word = word
+longest = longest + 2
+list_dict = list(word_dict.items())
+list_dict.sort(reverse=True, key=lambda pair: pair[1])
+
+for key, value in list_dict:
+    spaces = longest - len(key)
+    print(key, spaces * " ", value * '#')
