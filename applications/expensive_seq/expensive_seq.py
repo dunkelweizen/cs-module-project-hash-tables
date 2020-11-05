e19515384983c6ce71438aa776a09c4628bbecb0
Dunkelweizen <cai.nowicki@gmail.com>
2020-11-04 22:13:06 -0500

    expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,15 @@
-# Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
-
+    if x <= 0:
+        return y + z
+    if x > 0:
+        if (x,y,z) in cache.keys():
+            return cache[(x,y,z)]
+        else:
+            result = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+            cache.update({(x,y,z): result})
+            return result
 
 if __name__ == "__main__":
     for i in range(10):
