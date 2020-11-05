32fc1ab645e4944d35a908ad0bc2100bbeeda4ec
Dunkelweizen <cai.nowicki@gmail.com>
2020-11-04 22:48:50 -0500

    finished no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,16 @@
 def no_dups(s):
-    # Your code here
-
-
+    cache = {}
+    words = s.split()
+    to_pop = []
+    for i in range(len(words)):
+        if words[i] in cache.keys():
+            to_pop.append(i)
+        else:
+            cache[words[i]] = True
+    to_pop.sort(reverse=True)
+    for value in to_pop:
+        words.pop(value)
+    return " ".join(words)
 
 if __name__ == "__main__":
     print(no_dups(""))
