9be326104208d6eb5eae79ad8c46585ea78c903b
Dunkelweizen <cai.nowicki@gmail.com>
2020-11-05 22:30:15 -0500

    completed sum_diff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,6 +3,7 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
+from itertools import product
 #q = set(range(1, 10))
 #q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
@@ -11,5 +12,23 @@ q = (1, 3, 4, 7, 12)
 def f(x):
     return x * 4 + 6
 
-# Your code here
+cache = {}
+for value in q:
+    cache[value] = f(value)
+result = []
+combos = product(q,repeat=4)
+
+for combo in combos:
+    a = combo[0]
+    b = combo[1]
+    c = combo[2]
+    d = combo[3]
+    if cache[a] + cache[b] == cache[c] - cache[d]:
+        result.append([a,b,c,d])
+
+
+for combo in result:
+    print(f'f({combo[0]}) + f({combo[1]}) = f({combo[2]}) - f({combo[3]})')
+
+
 
